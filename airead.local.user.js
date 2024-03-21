513571cbf828b4a5e7ddf0cc138171065d0e6396
Hansimov <591172499@qq.com>
2024-03-21 23:52:03 +0800

    :gem: [Feature] Put openai-js in airead to enable GM_xmlhttpRequest

--- airead.local.user.js
+++ airead.local.user.js
@@ -8,9 +8,8 @@
 // @match        https://ar5iv.labs.arxiv.org/html/*
 // @match        http://127.0.0.1:17777/*.html
 // @icon         https://raw.githubusercontent.com/Hansimov/openai-js/main/penrose.png
+// @connect      *
 // @grant        GM_xmlhttpRequest
-// @grant        GM_getResourceText
-// @grant        GM_addStyle
 // ==/UserScript==
 
 function require_module(url, cache = true) {
@@ -53,21 +52,127 @@ function require_modules({ host = "127.0.0.1", port = 17777 } = {}) {
     let openai_user_js = `${server}/openai-js/openai.user.js`;
     let pure_page_user_js = `${server}/purepage/purepage.user.js`;
     let airead_user_js = `${server}/airead/airead.user.js`;
-    Promise.all([
+    return Promise.all([
         require_module(jquery_js),
         require_module(bootstrap_js),
         require_module(bootstrap_css),
         require_module(font_awesome_css),
         require_module(font_awesome_v4_css),
-        require_module(openai_user_js, false),
+        // require_module(openai_user_js, false),
         require_module(pure_page_user_js, false),
-    ])
-        .then(function () {
-            require_module(airead_user_js, false);
-        })
-        .catch(function (error) {
-            console.error(error);
+    ]);
+}
+
+const LLM_ENDPOINT = "https://hansimov-hf-llm-api.hf.space/api";
+
+async function process_stream_response(response, on_chunk) {
+    const decoder = new TextDecoder("utf-8");
+    function stringify_stream_bytes(bytes) {
+        return decoder.decode(bytes);
+    }
+    function jsonize_stream_data(data) {
+        var json_chunks = [];
+        data = data
+            .replace(/^data:\s*/gm, "")
+            .replace(/\[DONE\]/gm, "")
+            .split("\n")
+            .filter(function (line) {
+                return line.trim().length > 0;
+            })
+            .map(function (line) {
+                try {
+                    let json_chunk = JSON.parse(line.trim());
+                    json_chunks.push(json_chunk);
+                } catch {
+                    console.log(`Failed to parse: ${line}`);
+                }
+            });
+        return json_chunks;
+    }
+    let reader = response.response.getReader();
+    let content = "";
+
+    while (true) {
+        let { done, value } = await reader.read();
+        if (done) {
+            break;
+        }
+        let json_chunks = jsonize_stream_data(stringify_stream_bytes(value));
+        for (let json_chunk of json_chunks) {
+            let chunk = json_chunk.choices[0];
+            if (on_chunk) {
+                content += on_chunk(chunk);
+            }
+        }
+    }
+    return content;
+}
+
+function get_llm_models({ endpoint } = {}) {
+    return new Promise((resolve, reject) => {
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: endpoint + "/v1/models",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            onload: function (response) {
+                let data = JSON.parse(response.responseText);
+                let models = data.data.map((item) => item.id);
+                resolve(models);
+            },
+            onerror: function (error) {
+                reject(error);
+            },
         });
+    });
+}
+
+function chat_completions({
+    messages,
+    endpoint = LLM_ENDPOINT,
+    model = "mixtral-8x7b",
+    max_tokens = -1,
+    temperature = 0.5,
+    top_p = 0.95,
+    stream = false,
+} = {}) {
+    return new Promise((resolve, reject) => {
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: endpoint + "/v1/chat/completions",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            data: JSON.stringify({
+                model: model,
+                messages: messages,
+                max_tokens: max_tokens,
+                temperature: temperature,
+                top_p: top_p,
+                stream: stream,
+            }),
+            responseType: stream ? "stream" : "json",
+            onloadstart: function (response) {
+                if (stream) {
+                    resolve(response);
+                }
+            },
+            onload: function (response) {
+                if (!stream) {
+                    let data = JSON.parse(response.responseText);
+                    let content = data.choices[0].message.content;
+                    resolve(content);
+                }
+            },
+            onerror: function (error) {
+                reject(error);
+            },
+        });
+    });
+}
+
 }
 
 (function () {
