5c1303e949020b56b6bd85ab05dc0381b942d6d8
Hansimov <591172499@qq.com>
2024-03-21 16:06:47 +0800

    :gem: [Feature] require_module: support css, and promise all before load airead

--- airead.local.user.js
+++ airead.local.user.js
@@ -9,27 +9,60 @@
 // @match        http://127.0.0.1:17777/*.html
 // @icon         https://raw.githubusercontent.com/Hansimov/openai-js/main/penrose.png
 // @grant        GM_xmlhttpRequest
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
 // ==/UserScript==
 
-function require_module(url, callback = null) {
-    GM.xmlHttpRequest({
-        method: "GET",
-        url: url + `?ts=${new Date().getTime()}`,
-        onload: function (response) {
-            let script_element = document.createElement("script");
-            script_element.innerHTML = response.responseText;
-            document.body.appendChild(script_element);
-            if (callback) callback();
-        },
+function require_module(url, cache = true) {
+    return new Promise(function (resolve, reject) {
+        if (!cache) {
+            url = url + `?ts=${new Date().getTime()}`;
+        }
+        GM.xmlHttpRequest({
+            method: "GET",
+            url: url,
+            onload: function (response) {
+                let module_element;
+                if (url.endsWith(".css")) {
+                    module_element = document.createElement("style");
+                } else {
+                    module_element = document.createElement("script");
+                }
+                module_element.innerHTML = response.responseText;
+                document.head.appendChild(module_element);
+                console.log("Loading script:", url);
+                resolve();
+            },
+            onerror: function (error) {
+                reject(error);
+            },
+        });
     });
 }
 
 function require_modules({ host = "127.0.0.1", port = 17777 } = {}) {
+    let jquery_js = "https://code.jquery.com/jquery-3.7.1.min.js";
+    let bootstrap_js =
+        "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js";
+    let bootstrap_css =
+        "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css";
     let server = `http://${host}:${port}`;
-    require_module(`${server}/openai-js/openai.user.js`);
-    require_module(`${server}/purepage/purepage.user.js`, function () {
-        require_module(`${server}/airead/airead.user.js`);
-    });
+    let openai_user_js = `${server}/openai-js/openai.user.js`;
+    let pure_page_user_js = `${server}/purepage/purepage.user.js`;
+    let airead_user_js = `${server}/airead/airead.user.js`;
+    Promise.all([
+        require_module(jquery_js),
+        require_module(bootstrap_js),
+        require_module(bootstrap_css),
+        require_module(openai_user_js, false),
+        require_module(pure_page_user_js, false),
+    ])
+        .then(function () {
+            require_module(airead_user_js, false);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
 }
 
 (function () {
