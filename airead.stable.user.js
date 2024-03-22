bcb03a4a3c019985c218dca252f79464e8e21f5a
Hansimov <591172499@qq.com>
2024-03-22 14:58:56 +0800

    :gem: [Feature] Publish stable version of script and module

---
+++ airead.stable.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         AIRead
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  An AI-assisted reading script in browsers.
+// @author       Hansimov
+// @match        https://*.wikipedia.org/wiki/*
+// @match        https://ar5iv.labs.arxiv.org/html/*
+// @icon         https://raw.githubusercontent.com/Hansimov/airead/main/logo.png
+// @connect      *
+// @grant        GM_xmlhttpRequest
+// @require      https://github.com/Hansimov/airead/raw/main/airead_module.stable.user.js
+// ==/UserScript==
+
+(function () {
+    "use strict";
+})();
